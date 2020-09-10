45f28c48987d7b3028220d84af335f1bda58afe7
TheRealAstoo <louis.fanien@gmail.com>
2020-09-10 16:36:51 +0200

    finitooooooooooooooooo

---
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
@@ -0,0 +1,15 @@
+// Don't modify these lines for tests to work properly.
+const star = require("./data/starData");
+const planet = require("./data/exoplanetData");
+
+// Your code here:
+let canHabitateLife = false;
+
+if ((star.spectralClass === "K" || star.spectralClass === "M") && (planet.radius === 1 && planet.mass === 1) && (planet.mass < 3) && (planet.habitalZone) && (planet.rotationStability) || (planet.mass < 0.5 && planet.radius > 1))  { 
+        canHabitateLife = true;
+    } else {
+        canHabitateLife = false;
+    }
+
+// Don't modifiy this line for test to work properly.
+module.exports = canHabitateLife;
