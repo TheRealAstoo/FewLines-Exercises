67fa0ca0d90fa879477c8d90b085cc45370ae61e
TheRealAstoo <louis.fanien@gmail.com>
2020-09-11 18:09:57 +0200

    fibonacci finished

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/08_recursion_bonus/src/02_fibonacci.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/08_recursion_bonus/src/02_fibonacci.js
@@ -1,6 +1,18 @@
 function fibo(n) {
-  // your code here
+  const result = [0, 1];
+  if (n >= 0) {
+    for (let i = 2; i <= n; i++) {
+      const a = result[i - 1];
+      const b = result[i - 2];
+      result.push(a + b);
+    }
+  } else {
+    return null;
+  }
+
+  return result[n];
 }
 
+console.log(fibo(10));
 // do not remove this line, it is for tests
 module.exports = fibo;
