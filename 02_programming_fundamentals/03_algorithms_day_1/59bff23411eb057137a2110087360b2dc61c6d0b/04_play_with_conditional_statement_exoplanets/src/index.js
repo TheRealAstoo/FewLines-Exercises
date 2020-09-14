18cdf392f59fd62aca6afc038f4cfefe73d41229
TheRealAstoo <louis.fanien@gmail.com>
2020-09-14 11:06:04 +0200

    pushed some changes that were not applied

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
@@ -5,7 +5,7 @@ const planet = require("./data/exoplanetData");
 // Your code here:
 let canHabitateLife = false;
 
-if ((star.spectralClass === "K" || star.spectralClass === "M") && (planet.radius === 1 && planet.mass === 1) && (planet.mass < 3) && (planet.habitalZone) && (planet.rotationStability) || (planet.mass < 0.5 && planet.radius > 1))  { 
+if ((star.spectralClass === "K" || star.spectralClass === "M") && (planet.radius === 1 && planet.mass === 1) && (planet.mass < 3) && (planet.habitalZone) && (planet.rotationStability) && (planet.mass > 0.5 && planet.radius > 1) || (planet.mass < 0.5 && planet.radius > 1))  { 
         canHabitateLife = true;
     } else {
         canHabitateLife = false;
--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/08_recursion_bonus/src/03_pow.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/08_recursion_bonus/src/03_pow.js
@@ -2,17 +2,16 @@ function pow(number, power) {
   let f = [];
   let calc;
 
-  
   for (let i = 1; i <= power; i++) {
     f.push(number);
   }
 
-  if ((power === 0) || (power === 1)) {
+  if (power === 0 || power === 1) {
     calc = 1;
   }
 
   if (f.length > 0) {
-    calc = f.reduce(function(accumulator, currentValue) {
+    calc = f.reduce(function (accumulator, currentValue) {
       return accumulator * currentValue;
     });
   }
